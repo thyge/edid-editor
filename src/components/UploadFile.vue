f6c4e199f791f098cf224558981a7afb26c1da11
Thyge Haarberg <thygehaarberg@TMBP.local>
2022-07-10 15:07:51 +0100

    refactor upload logic into upload file, only handle bytes in main app

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -1,14 +1,34 @@
 <script>
 export default {
-    props: {
-        mEdid: Object
-    },
-    methods: {
-        uploadFile: function(e) {
-            let file = e.target.files[0];
-            this.$emit('ParseEEDID', file)
+  props: {
+    mEdid: Object
+  },
+  emits: ["ParseEEDID"],
+  methods: {
+    uploadFile: function(e) {
+      let file = e.target.files[0];
+      if ((typeof file === 'undefined') || file === null) {
+        return
+      }
+      let reader = new FileReader();
+      let vm = this
+      if (file.name.split('.').pop() === "bin") {
+        reader.onload = function(event) {
+          let edidarr = new Uint8Array(event.target.result);
+          vm.$emit('ParseEEDID', edidarr)
         }
+        reader.readAsArrayBuffer(file);
+      } else if (file.name.split('.').pop() === "txt") {
+        reader.onload = function(event) {
+          let txtEdid = event.target.result.replaceAll(",", "")
+          txtEdid = txtEdid.replaceAll(" ", "")
+          let edidarr = Uint8Array.from(txtEdid.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+          vm.$emit('ParseEEDID', edidarr)
+        }
+        reader.readAsText(file);
+      }
     }
+  }
 }
 </script>
 
