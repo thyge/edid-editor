c9f3ba83283dafb9c347b2fcb5fe58a959983cc9
Thyge Haarberg <thygehaarberg@TMBP.local>
2023-03-27 21:32:28 +0200

    add milligram styling

--- src/components/CEADatablocksView.vue
+++ src/components/CEADatablocksView.vue
@@ -19,95 +19,133 @@ export default {
 
 <template>
 <div v-for="db in datablocks" :key="db.id">
-  <div v-if="db.Header.Type === 'DBAudioDataBlock'">
-    <h4>Audio Data Block</h4>
-    <button @click="RemoveElement(db.id)">remove</button>
-    <table>
-      <tr>
-        <td>Type</td>
+  <div class="container" v-if="db.Header.Type === 'DBAudioDataBlock'">
+    <div class="row">
+      <div class="column column-50">
+        <h4>Audio Data Block</h4>
+      </div>
+      <div class="column">
+        <button @click="RemoveElement(db.id)">remove</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column column-10">Type</div>
+      <div class="column column-20">
         <select v-model="db.Content.AudioType">
           <option>LPCM</option>
         </select>
-      </tr>
-      <tr>
-        <td>Channels</td>
-        <td><input type="number" v-model="db.Content.Channels" disabled/></td>
-      </tr>
-    </table>
-    <table>
-      <tr>
-        <td></td>
-        <td>192Khz</td>
-        <td>176Khz</td>
-        <td>96Khz</td>
-        <td>88Khz</td>
-        <td>48Khz</td>
-        <td>44.1Khz</td>
-        <td>32Khz</td>
-      </tr>
-      <tr>
-        <td>Sampling</td>
-        <td><input type="checkbox" v-model="db.Content.Sampling192" disabled/></td>
-        <td><input type="checkbox" v-model="db.Content.Sampling176" disabled/></td>
-        <td><input type="checkbox" v-model="db.Content.Sampling96" disabled/></td>
-        <td><input type="checkbox" v-model="db.Content.Sampling88" disabled/></td>
-        <td><input type="checkbox" v-model="db.Content.Sampling48" disabled/></td>
-        <td><input type="checkbox" v-model="db.Content.Sampling44_1" disabled/></td>
-        <td><input type="checkbox" v-model="db.Content.Sampling32" disabled/></td>
-      </tr>
-      </table>
-      <table>
-      <tr>
-        <td></td>
-        <td>16</td>
-        <td>20</td>
-        <td>24</td>
-      </tr>
-      <tr>
-        <td>BitDepth</td>
-        <td><input type="checkbox" v-model="db.Content.BitDepth16" disabled/></td>
-        <td><input type="checkbox" v-model="db.Content.BitDepth20" disabled/></td>
-        <td><input type="checkbox" v-model="db.Content.BitDepth24" disabled/></td>
-      </tr>
-    </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column column-10">
+        Channels
+      </div>
+      <div class="column column-20">
+        <input type="number" v-model="db.Content.Channels" disabled/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column column-40">
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>192Khz</th>
+              <th>176Khz</th>
+              <th>96Khz</th>
+              <th>88Khz</th>
+              <th>48Khz</th>
+              <th>44.1Khz</th>
+              <th>32Khz</th>
+            </tr>
+          </thead>
+        <tbody>
+        </tbody>
+          <tr>
+            <td>Sampling</td>
+            <td><input type="checkbox" v-model="db.Content.Sampling192" disabled/></td>
+            <td><input type="checkbox" v-model="db.Content.Sampling176" disabled/></td>
+            <td><input type="checkbox" v-model="db.Content.Sampling96" disabled/></td>
+            <td><input type="checkbox" v-model="db.Content.Sampling88" disabled/></td>
+            <td><input type="checkbox" v-model="db.Content.Sampling48" disabled/></td>
+            <td><input type="checkbox" v-model="db.Content.Sampling44_1" disabled/></td>
+            <td><input type="checkbox" v-model="db.Content.Sampling32" disabled/></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column column-40">
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>16</th>
+              <th>20</th>
+              <th>24</th>
+            </tr>
+          </thead>
+        <tbody>
+        </tbody>
+          <tr>
+            <td>BitDepth</td>
+            <td><input type="checkbox" v-model="db.Content.BitDepth16" disabled/></td>
+            <td><input type="checkbox" v-model="db.Content.BitDepth20" disabled/></td>
+            <td><input type="checkbox" v-model="db.Content.BitDepth24" disabled/></td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
-  <div v-else-if="db.Header.Type === 'DBVideoDataBlock'">
-    <h4>Video Data Blocks</h4>
+  <div class="container" v-else-if="db.Header.Type === 'DBVideoDataBlock'">
+    <div class="row">
+      <div class="column">
+        <h4>Video Data Blocks</h4>
+      </div>
+    </div>
     <VideoDataBlock :m_vics="db.Content.VICs"/>
   </div>
-  <div v-else-if="db.Header.Type === 'DBVendorSpecificDataBlock'">
+  <div class="container" v-else-if="db.Header.Type === 'DBVendorSpecificDataBlock'">
     <VSDBView :m_datablock="db.Content"/>
   </div>
-  <div v-else-if="db.Header.Type === 'DBSpeakerAllocationData'">
-    <h4>Speaker Allocation Data Block</h4>
-    <table>
-      <tr>
-        <td>RearLeftRightCenter</td>
-        <td><input type="checkbox" v-model="db.Content.RearLeftRightCenter" disabled/></td>
-      </tr>
-      <tr>
-        <td>RearCenter</td>
-        <td><input type="checkbox" v-model="db.Content.RearCenter" disabled/></td>
-      </tr>
-      <tr>
-        <td>RearLeftRight</td>
-        <td><input type="checkbox" v-model="db.Content.RearLeftRight" disabled/></td>
-      </tr>
-      <tr>
-        <td>FrontCenter</td>
-        <td><input type="checkbox" v-model="db.Content.FrontCenter" disabled/></td>
-      </tr>
-      <tr>
-        <td>LFE</td>
-        <td><input type="checkbox" v-model="db.Content.LFE" disabled/></td>
-      </tr>
-      <tr>
-        <td>FrontLeftRight</td>
-        <td><input type="checkbox" v-model="db.Content.FrontLeftRight" disabled/></td>
-      </tr>
-    </table>
+  <div class="container" v-else-if="db.Header.Type === 'DBSpeakerAllocationData'">
+    <div class="row">
+      <div class="column">
+        <h4>Speaker Allocation Data Block</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column column-10">
+        <table>
+          <tr>
+            <td>RearLeftRightCenter</td>
+            <td><input type="checkbox" v-model="db.Content.RearLeftRightCenter" disabled/></td>
+          </tr>
+          <tr>
+            <td>RearCenter</td>
+            <td><input type="checkbox" v-model="db.Content.RearCenter" disabled/></td>
+          </tr>
+          <tr>
+            <td>RearLeftRight</td>
+            <td><input type="checkbox" v-model="db.Content.RearLeftRight" disabled/></td>
+          </tr>
+          <tr>
+            <td>FrontCenter</td>
+            <td><input type="checkbox" v-model="db.Content.FrontCenter" disabled/></td>
+          </tr>
+          <tr>
+            <td>LFE</td>
+            <td><input type="checkbox" v-model="db.Content.LFE" disabled/></td>
+          </tr>
+          <tr>
+            <td>FrontLeftRight</td>
+            <td><input type="checkbox" v-model="db.Content.FrontLeftRight" disabled/></td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
-  <div v-else-if="db.Header.Type === 'DBUseExtendedTag'">
+  <div class="container" v-else-if="db.Header.Type === 'DBUseExtendedTag'">
     <CEAExtendedTagView :m_datablock="db.Content"/>
   </div>
   <div v-else-if="db.Header.Type === 'DetailedTimingDescriptor'">
@@ -121,9 +159,9 @@ export default {
 </template>
 
 <style scoped>
-table, th, td {
+/* table, th, td {
   border: 1px solid;
   border-collapse: collapse;
   margin: 5px;
-}
+} */
 </style>
--- src/components/CEAExtendedTagView.vue
+++ src/components/CEAExtendedTagView.vue
@@ -19,109 +19,123 @@ export default {
 </script>
 
 <template>
-<h4>{{datablock.ExtendedName}}</h4>
 <div v-if="datablock.ExtendedName === 'ColorimetryDB'">
-<table>
-  <tr>
-    <td>xvYCC601</td>
-    <td><input type="checkbox" v-model="datablock.xvYCC601" disabled/></td>
-  </tr>
-  <tr>
-    <td>xvYCC709</td>
-    <td><input type="checkbox" v-model="datablock.xvYCC709" disabled/></td>
-  </tr>
-  <tr>
-    <td>sYCC601</td>
-    <td><input type="checkbox" v-model="datablock.sYCC601" disabled/></td>
-  </tr>
-  <tr>
-    <td>opYCC601</td>
-    <td><input type="checkbox" v-model="datablock.opYCC601" disabled/></td>
-  </tr>
-  <tr>
-    <td>opRGB</td>
-    <td><input type="checkbox" v-model="datablock.opRGB" disabled/></td>
-  </tr>
-  <tr>
-    <td>BT2020cYCC</td>
-    <td><input type="checkbox" v-model="datablock.BT2020cYCC" disabled/></td>
-  </tr>
-  <tr>
-    <td>BT2020YCC</td>
-    <td><input type="checkbox" v-model="datablock.BT2020YCC" disabled/></td>
-  </tr>
-  <tr>
-    <td>BT2020RGB</td>
-    <td><input type="checkbox" v-model="datablock.BT2020RGB" disabled/></td>
-  </tr>
-  <tr>
-    <td>DCIP3</td>
-    <td><input type="checkbox" v-model="datablock.DCIP3" disabled/></td>
-  </tr>
-</table>
+  <h4>{{datablock.ExtendedName}}</h4>
+  <div class="row">
+    <div class="column column-10">
+    <table>
+      <tr>
+        <td>xvYCC601</td>
+        <td><input type="checkbox" v-model="datablock.xvYCC601" disabled/></td>
+      </tr>
+      <tr>
+        <td>xvYCC709</td>
+        <td><input type="checkbox" v-model="datablock.xvYCC709" disabled/></td>
+      </tr>
+      <tr>
+        <td>sYCC601</td>
+        <td><input type="checkbox" v-model="datablock.sYCC601" disabled/></td>
+      </tr>
+      <tr>
+        <td>opYCC601</td>
+        <td><input type="checkbox" v-model="datablock.opYCC601" disabled/></td>
+      </tr>
+      <tr>
+        <td>opRGB</td>
+        <td><input type="checkbox" v-model="datablock.opRGB" disabled/></td>
+      </tr>
+      <tr>
+        <td>BT2020cYCC</td>
+        <td><input type="checkbox" v-model="datablock.BT2020cYCC" disabled/></td>
+      </tr>
+      <tr>
+        <td>BT2020YCC</td>
+        <td><input type="checkbox" v-model="datablock.BT2020YCC" disabled/></td>
+      </tr>
+      <tr>
+        <td>BT2020RGB</td>
+        <td><input type="checkbox" v-model="datablock.BT2020RGB" disabled/></td>
+      </tr>
+      <tr>
+        <td>DCIP3</td>
+        <td><input type="checkbox" v-model="datablock.DCIP3" disabled/></td>
+      </tr>
+    </table>
+  </div>
+  </div>
 </div>
 <div v-else-if="datablock.ExtendedName === 'VideoCapabilityDB'">
-<table>
-  <tr>
-    <td>YCCQuantizationRangeSelectable</td>
-    <td><input type="checkbox" v-model="datablock.YCCQuantizationRangeSelectable" disabled/></td>
-  </tr>
-  <tr>
-    <td>RGBQuantizationRangeSelectable</td>
-    <td><input type="checkbox" v-model="datablock.RGBQuantizationRangeSelectable" disabled/></td>
-  </tr>
-  <tr>
-    <td>PTOverscanBehavior</td>
-    <td>{{datablock.PTOverscanBehavior}}</td>
-  </tr>
-  <tr>
-    <td>ITOverscanBehavior</td>
-    <td>{{datablock.ITOverscanBehavior}}</td>
-  </tr>
-  <tr>
-    <td>CEOverscanBehavior</td>
-    <td>{{datablock.CEOverscanBehavior}}</td>
-  </tr>
-</table>
+  <h4>{{datablock.ExtendedName}}</h4>
+  <div class="row">
+    <div class="column column-10">
+      <table>
+        <tr>
+          <td>YCCQuantizationRangeSelectable</td>
+          <td><input type="checkbox" v-model="datablock.YCCQuantizationRangeSelectable" disabled/></td>
+        </tr>
+        <tr>
+          <td>RGBQuantizationRangeSelectable</td>
+          <td><input type="checkbox" v-model="datablock.RGBQuantizationRangeSelectable" disabled/></td>
+        </tr>
+        <tr>
+          <td>PTOverscanBehavior</td>
+          <td>{{datablock.PTOverscanBehavior}}</td>
+        </tr>
+        <tr>
+          <td>ITOverscanBehavior</td>
+          <td>{{datablock.ITOverscanBehavior}}</td>
+        </tr>
+        <tr>
+          <td>CEOverscanBehavior</td>
+          <td>{{datablock.CEOverscanBehavior}}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
 </div>
 <div v-else-if="datablock.ExtendedName === 'YCBCR420CapabilityMap'">
   <VideoDataBlock :m_vics="datablock.VICs"/>
 </div>
 <div v-else-if="datablock.ExtendedName === 'HDRStaticMetadataDB'">
-<table>
-  <tr>
-    <td>SDR</td>
-    <td><input type="checkbox" v-model="datablock.SDR" disabled/></td>
-  </tr>
-  <tr>
-    <td>HDR</td>
-    <td><input type="checkbox" v-model="datablock.HDR" disabled/></td>
-  </tr>
-  <tr>
-    <td>HLG</td>
-    <td><input type="checkbox" v-model="datablock.HLG" disabled/></td>
-  </tr>
-  <tr>
-    <td>ST2084</td>
-    <td><input type="checkbox" v-model="datablock.ST2084" disabled/></td>
-  </tr>
-  <tr>
-    <td>StaticMetadataType1</td>
-    <td><input type="checkbox" v-model="datablock.StaticMetadataType1" disabled/></td>
-  </tr>
-  <tr>
-    <td>Max luminance cd/m^2</td>
-    <td><input type="number" v-model.number="datablock.ContentMaxLuminanceData" disabled/></td>
-  </tr>
-  <tr>
-    <td>Max frame-average luminance cd/m^2</td>
-    <td><input type="number" v-model.number="datablock.ContentMaxFrameAverage" disabled/></td>
-  </tr>
-  <tr>
-    <td>Min luminance cd/m^2</td>
-    <td><input type="number" v-model.number="datablock.ContentMinLuminance" disabled/></td>
-  </tr>
-</table>
+  <h4>{{datablock.ExtendedName}}</h4>
+  <div class="row">
+    <div class="column column-10">
+      <table>
+        <tr>
+          <td>SDR</td>
+          <td><input type="checkbox" v-model="datablock.SDR" disabled/></td>
+        </tr>
+        <tr>
+          <td>HDR</td>
+          <td><input type="checkbox" v-model="datablock.HDR" disabled/></td>
+        </tr>
+        <tr>
+          <td>HLG</td>
+          <td><input type="checkbox" v-model="datablock.HLG" disabled/></td>
+        </tr>
+        <tr>
+          <td>ST2084</td>
+          <td><input type="checkbox" v-model="datablock.ST2084" disabled/></td>
+        </tr>
+        <tr>
+          <td>StaticMetadataType1</td>
+          <td><input type="checkbox" v-model="datablock.StaticMetadataType1" disabled/></td>
+        </tr>
+        <tr>
+          <td>Max luminance cd/m^2</td>
+          <td><input type="number" v-model.number="datablock.ContentMaxLuminanceData" disabled/></td>
+        </tr>
+        <tr>
+          <td>Max frame-average luminance cd/m^2</td>
+          <td><input type="number" v-model.number="datablock.ContentMaxFrameAverage" disabled/></td>
+        </tr>
+        <tr>
+          <td>Min luminance cd/m^2</td>
+          <td><input type="number" v-model.number="datablock.ContentMinLuminance" disabled/></td>
+        </tr>
+      </table>
+    </div>
+  </div>
 </div>
 <div v-else>
 {{datablock}}
@@ -129,8 +143,4 @@ export default {
 </template>
 
 <style scoped>
-table, th, td {
-  border: 1px solid;
-  border-collapse: collapse;
-}
 </style>
