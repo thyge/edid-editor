0ca0e7d8385be2301eca75dbbdf9ef0ef6c5c436
Thyge Haarberg <thygehaarberg@TMBP.local>
2024-09-16 12:58:35 +0200

    cards to grids for consistency in layout

--- src/components/edid/VideoInputDefinition.vue
+++ src/components/edid/VideoInputDefinition.vue
@@ -13,7 +13,6 @@ import {
 } from "@/components/ui/select";
 import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
-import { takeCoverage } from "v8";
 const videoInDef = edidstore.mEEDID.EDID.VideoInputDefinition;
 watch(
   () => edidstore.mEEDID.EDID.VideoInputDefinition.VideoSignalInterface,
@@ -24,14 +23,10 @@ watch(
 </script>
 
 <template>
-  <Card>
-    <CardHeader>
-      <CardTitle class="text-sm">Video Input Definition</CardTitle>
-    </CardHeader>
-    <CardContent>
-      <Label id="digital-mode">Video Signal Interface</Label>
+  <div class="grid grid-cols-3 gap-2 m-4">
+    <div>Video Signal Interface</div>
+    <div>
       <Select
-        id="digital-mode"
         v-model="videoInDef.VideoSignalInterface"
         @change="edidstore.setHeader"
       >
@@ -43,75 +38,69 @@ watch(
           <SelectItem value="Digital">Digital</SelectItem>
         </SelectContent>
       </Select>
-      <Table v-if="videoInDef.VideoSignalInterface === 'Digital'">
-        <TableBody>
-          <TableRow>
-            <TableCell> Display Bitdepth </TableCell>
-            <TableCell>
-              <Select v-model="videoInDef.BitDepth">
-                <SelectTrigger :disabled="edidstore.mEEDID.EDID.Revision < 4">
-                  <SelectValue></SelectValue>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="undefined">undefined</SelectItem>
-                  <SelectItem value="6">6</SelectItem>
-                  <SelectItem value="8">8</SelectItem>
-                  <SelectItem value="10">10</SelectItem>
-                  <SelectItem value="12">12</SelectItem>
-                  <SelectItem value="16">16</SelectItem>
-                </SelectContent>
-              </Select>
-            </TableCell>
-            <TableCell> EDID 1.4 Only </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell> Display Interface </TableCell>
-            <TableCell>
-              <Select v-model="edidstore.mEEDID.EDID.VideoInterface">
-                <SelectTrigger :disabled="edidstore.mEEDID.EDID.Revision < 4">
-                  <SelectValue></SelectValue>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="undefined">undefined</SelectItem>
-                  <SelectItem value="DVI">DVI</SelectItem>
-                  <SelectItem value="HDMIa">HDMIa</SelectItem>
-                  <SelectItem value="HDMIb">HDMIb</SelectItem>
-                  <SelectItem value="MDDI">MDDI</SelectItem>
-                  <SelectItem value="DisplayPort">DisplayPort</SelectItem>
-                </SelectContent>
-              </Select>
-            </TableCell>
-            <TableCell> EDID 1.4 Only </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell> RGB 4:4:4 + YCrCb 4:4:4 </TableCell>
-            <TableCell>
-              <Switch
-                :disabled="edidstore.mEEDID.EDID.Revision < 4"
-                v-model="edidstore.mEEDID.EDID.ColourEncoding.YUV444"
-              />
-            </TableCell>
-            <TableCell> EDID 1.4 Only </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell> RGB 4:4:4 + YCrCb 4:2:2 </TableCell>
-            <TableCell>
-              <Switch
-                :disabled="edidstore.mEEDID.EDID.Revision < 4"
-                v-model="edidstore.mEEDID.EDID.ColourEncoding.YUV422"
-              />
-            </TableCell>
-            <TableCell> EDID 1.4 Only </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-      <Table v-else-if="videoInDef.VideoSignalInterface === 'Analog'">
-        <TableBody>
-          <TableRow>
-            <TableCell> Analog not supported </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </CardContent>
-  </Card>
+    </div>
+  </div>
+
+  <div
+    v-if="videoInDef.VideoSignalInterface === 'Digital'"
+    class="grid grid-cols-3 gap-2 p-4 m-4 border rounded"
+  >
+    <div>Display Bitdepth</div>
+    <div>
+      <Select
+        id="select-bitdepth"
+        v-model="videoInDef.BitDepth"
+        @change="edidstore.setHeader"
+      >
+        <SelectTrigger :disabled="edidstore.mEEDID.EDID.Revision < 4">
+          <SelectValue></SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="undefined">undefined</SelectItem>
+          <SelectItem value="6">6</SelectItem>
+          <SelectItem value="8">8</SelectItem>
+          <SelectItem value="10">10</SelectItem>
+          <SelectItem value="12">12</SelectItem>
+          <SelectItem value="16">16</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+    <div>EDID 1.4 Only</div>
+    <div>Display Interface</div>
+    <div>
+      <Select v-model="edidstore.mEEDID.EDID.VideoInterface">
+        <SelectTrigger :disabled="edidstore.mEEDID.EDID.Revision < 4">
+          <SelectValue></SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="undefined">undefined</SelectItem>
+          <SelectItem value="DVI">DVI</SelectItem>
+          <SelectItem value="HDMIa">HDMIa</SelectItem>
+          <SelectItem value="HDMIb">HDMIb</SelectItem>
+          <SelectItem value="MDDI">MDDI</SelectItem>
+          <SelectItem value="DisplayPort">DisplayPort</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+    <div>EDID 1.4 Only</div>
+    <div>RGB 4:4:4 + YCrCb 4:4:4</div>
+    <div>
+      <Switch
+        :disabled="edidstore.mEEDID.EDID.Revision < 4"
+        v-model="edidstore.mEEDID.EDID.ColourEncoding.YUV444"
+      />
+    </div>
+    <div>EDID 1.4 Only</div>
+    <div>RGB 4:4:4 + YCrCb 4:2:2</div>
+    <div>
+      <Switch
+        :disabled="edidstore.mEEDID.EDID.Revision < 4"
+        v-model="edidstore.mEEDID.EDID.ColourEncoding.YUV422"
+      />
+    </div>
+    <div>EDID 1.4 Only</div>
+  </div>
+  <div v-else-if="videoInDef.VideoSignalInterface === 'Analog'" class="grid grid-cols-3 gap-2 p-4 m-4 border rounded">
+      <div>Analog not supported</div>
+  </div>
 </template>
