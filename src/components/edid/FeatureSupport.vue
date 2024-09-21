41b3f24f84716253d3b92c5ef4f78cbb7a6edc85
Thyge Haarberg <thygehaarberg@TMBP.local>
2024-09-21 19:31:52 +0200

    @update works where @change fails

--- src/components/edid/FeatureSupport.vue
+++ src/components/edid/FeatureSupport.vue
@@ -2,30 +2,19 @@
 import { useEdidStore } from "@/stores/edidStore";
 const edidstore = useEdidStore();
 import { Switch } from "@/components/ui/switch";
-import { watch } from "vue";
-watch(
-  () => edidstore.mEEDID.EDID.ContiniousFrequency,
-  () => {
-    edidstore.setHeader();
-  }
-);
-function onChanges() {
-  console.log("onChanges");
-}
 </script>
 
 <template>
-  
   <div class="grid grid-cols-3 gap-2 m-4">
     <div>Feature Support</div>
   </div>
-  <div class="grid grid-cols-3 gap-2 m-4 p-4  border rounded">
+  <div class="grid grid-cols-3 gap-2 m-4 p-4 border rounded">
     <div>DPMS Standby</div>
     <div>
       <Switch
         :disabled="edidstore.mEEDID.EDID.Revision < 4"
         v-model:checked="edidstore.mEEDID.EDID.DPMSstandby"
-        @change="onChanges"
+        @update:checked="edidstore.setHeader()"
       />
     </div>
     <div>EDID 1.4 Only</div>
@@ -34,6 +23,7 @@ function onChanges() {
       <Switch
         :disabled="edidstore.mEEDID.EDID.Revision < 4"
         v-model:checked="edidstore.mEEDID.EDID.DPMSsuspend"
+        @update:checked="edidstore.setHeader()"
       />
     </div>
     <div>EDID 1.4 Only</div>
@@ -42,28 +32,37 @@ function onChanges() {
       <Switch
         :disabled="edidstore.mEEDID.EDID.Revision < 4"
         v-model:checked="edidstore.mEEDID.EDID.DPMSactiveOff"
+        @update:checked="edidstore.setHeader()"
       />
     </div>
     <div>EDID 1.4 Only</div>
     <div>Native Pixel Format in Preferred Timing Mode</div>
     <div>
-      <Switch v-model:checked="edidstore.mEEDID.EDID.PreferredTiming" @change="onChanges" />
+      <Switch
+        v-model:checked="edidstore.mEEDID.EDID.PreferredTiming"
+        @update:checked="edidstore.setHeader()"
+      />
     </div>
     <div></div>
     <template v-if="edidstore.mEEDID.EDID.Revision < 4">
       <div>GTF Support</div>
       <div>
-        <Switch v-model:checked="edidstore.mEEDID.EDID.GTFSupport" />
+        <Switch
+          v-model:checked="edidstore.mEEDID.EDID.GTFSupport"
+          @update:checked="edidstore.setHeader()"
+        />
       </div>
       <div>No encoding support</div>
     </template>
     <template v-if="edidstore.mEEDID.EDID.Revision > 3">
       <div>Continuous Frequency</div>
       <div>
-        <Switch v-model:checked="edidstore.mEEDID.EDID.ContiniousFrequency" />
+        <Switch
+          v-model:checked="edidstore.mEEDID.EDID.ContiniousFrequency"
+          @update:checked="edidstore.setHeader()"
+        />
       </div>
       <div>No encoding support</div>
     </template>
-    
   </div>
 </template>
